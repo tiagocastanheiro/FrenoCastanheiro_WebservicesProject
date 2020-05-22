8922e62cc48939d9b9aced6e2a84db76f5c6abf1
GabrielleFreno01 <freno.gabrielle@gmail.com>
2020-05-22 14:41:13 +0200

    DTO

---
+++ DTO/PaymentDatabaseTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PaymentDatabaseTransaction
+    {
+        public int Id { get; set; }
+        public int UID { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DTO/SAPHR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class SAPHR
+    {
+        public int Id { get; set; }
+        public int UID { get; set; }
+        public string Username { get; set; }
+        public int CardID { get; set; }
+    }
+}
